94d4146d3dbb94c6432310db22158cc232cf6440
Tusharshrivastav <ts3657@gmail.com>
2021-11-16 16:50:00 +0530

    make user and and hash user password

--- app/Controllers/Http/SignUpController.ts
+++ app/Controllers/Http/SignUpController.ts
@@ -1,8 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User';
 export default class SignUpController {
-  public async index({ request }: HttpContextContract) {
-    await request.validate({
+  public async index({ request, response }: HttpContextContract) {
+    const req = await request.validate({
       schema: schema.create({
         name: schema.string(),
         email: schema.string({}, [rules.email()]),
@@ -16,6 +17,14 @@ export default class SignUpController {
         "password.required": "Password is required to sign up",
       },
     });
+
+    const user = new User()
+    user.name = req.name
+    user.email = req.email
+    user.password = req.password
+    await user.save()
+    return response.redirect('/')
+
   }
 
   public async create({ }: HttpContextContract) { }
